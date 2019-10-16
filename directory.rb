aa9beee3f9db6240b52e5d8d0f866e9b68d1c9d5
Selina Erhabor <selinaerhabor@gmail.com>
2019-10-16 15:53:18 +0100

    Added indexing to student names

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
 student_count = 11
 #first we print the list of students
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked witch of the west",
+  "Terminator",
+  "Freddie Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 puts "The Students of Villians Academy"
 puts "-----------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked witch of the west"
-puts "Terminator"
-puts "Freddie Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
 #finally we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
