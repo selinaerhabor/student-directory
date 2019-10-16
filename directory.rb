c060c28578f3c2921b02c878a83d5c1342d10683
Selina Erhabor <selinaerhabor@gmail.com>
2019-10-16 15:58:38 +0100

    Used iteration to call student names

--- directory.rb
+++ directory.rb
@@ -15,16 +15,8 @@ students = [
 ]
 puts "The Students of Villians Academy"
 puts "-----------------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each do |student|
+    puts student
+end
 #finally we print the total number of students
 puts "Overall, we have #{students.count} great students"
